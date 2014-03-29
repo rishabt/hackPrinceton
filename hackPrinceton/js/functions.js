0edbc6c9a2f6bd86f2b77656b5dee6a48920d744
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-03-29 08:49:37 -0400

    working prototype for the front end

--- hackPrinceton/js/functions.js
+++ hackPrinceton/js/functions.js
@@ -20,6 +20,59 @@ $(document).ready(function(){
         div.on('click', function(){
             $("#page_44_28").show();
             $("#bookShelf").hide();
+            
+            
+            var sprayReader = new SprayReader('#spray_result');
+
+        $(document).ready(function() {
+            running = false;
+            isPaused = false;
+          $('#start').click(function(event) {
+              if(!running && !isPaused){
+                running=true;
+                isPaused=false;
+                var inputText = text;
+                var wpm = $('#wpm').val();
+    
+                sprayReader.setInput(inputText);
+                sprayReader.setWpm(wpm);
+                sprayReader.start(); 
+
+                event.preventDefault();
+                  $("#start").text("");
+                  $("#start").text("pause");
+              }
+              else if (isPaused && !running){//Resume function
+                    running = true;
+                  isPaused = false;
+                  sprayReader.resume();
+                    event.preventDefault();
+                $("#start").text("");
+                  $("#start").text("pause");
+              }
+              else if (!isPaused && running){
+                running = false;
+                isPaused = true;
+                sprayReader.pause();
+                event.preventDefault();
+                  $("#start").text("");
+                  $("#start").text("start");
+              }
+              else{
+                 running = false;
+                  isPaused = true;
+              }
+          });
+
+          $('#stop').click(function(event) {
+                running = false;
+                isPaused = false;
+                sprayReader.stop();
+                event.preventDefault();
+                $("#start").text("");
+                $("#start").text("start");
+          });
+        });
         });
 
         $("#testShelf").append(div);  
