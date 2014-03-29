3e951cecbbb12dc7bc9aeb5d5b8718fe9278f9e2
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-03-29 16:24:58 -0400

    Fix up some of the shotty hackathon style code

--- hackPrinceton/js/functions.js
+++ hackPrinceton/js/functions.js
@@ -1,26 +1,24 @@
 $(document).ready(function(){
-    $("#page_44_28").hide();
+    var sprayReader = new SprayReader('#spray_result');
+    
     $("#bookShelf").show();
+    $("#page_44_28").hide();
     $("#newBook").hide();  
     $("#parser").hide();
-
-    $("#parse").on('click', function(){
-        //TODO for the parser, needs to return the author, title, content, and word count which will be asynch posted to the database
-    });
     
     jQuery.extend({
-    getValues: function(url) {
-        var result = null;
-        $.ajax({
-            url: url,
-            type: "GET",
-            contentType: "application/json",
-            async: false,
-            success: function(data) {
-                result = data;
-            }
-        });
-        return result;
+        getValues: function(url) {
+            var result = null;
+            $.ajax({
+                url: url,
+                type: "GET",
+                contentType: "application/json",
+                async: false,
+                success: function(data) {
+                    result = data;
+                }
+            });
+            return result;
         }
     });  
     
@@ -40,106 +38,107 @@ $(document).ready(function(){
     
     $("#testShelf").append(firstDiv); 
     
-    $.each(results[0].book, function(index, element){
-        var author = element.author;
-        var title = element.title;
-        var source = element.source;
-        var wordcount = element.wordcount;
-        var text = element.text;
-        
-        
+    function divActions(author, title, source, wordcount, text){
+        this.author = author;
+        this.title = title;
+        this.source = source;
+        this.wordcount = wordcount;
+        this.text = text;
+        this.running = false;
+        this.isPaused = false;
+       
         var div = $('<li class="divider"></li><li data-uib="app_framework/listitem"><a>'+title+'</a><span id=author>'+author+'<span></li>');
+        
         div.css({
            'cursor':'pointer', 
         });
-
+        
         div.on('click', function(){
             $("#page_44_28").show();
             $("#bookShelf").hide();
             
-            var sprayReader = new SprayReader('#spray_result');
-            
-        $(document).ready(function() {
-            
-                $("#upSpeed").on("click", function(){
-                    sprayReader.pause();
-                       wpm = $("#speed").text();
-                       wpm = parseInt(wpm, 10);
-                       if (wpm<850) wpm+=50;
-                       $("#speed").text(wpm);
-                        sprayReader.setWpm(wpm);
-                    sprayReader.resume();
-                   });
+            var inputText = text;
+            var wpm = $('#wpm').val();
+            sprayReader.setInput(inputText);
+            sprayReader.setWpm(wpm);
+        });
+        
+        $("#testShelf").append(div);
+    }
+    
+    $.each(results[0].book, function(index, element){
+        this.div = new divActions(element.author, element.title, element.source, element.wordcount, element.text);
+    });
 
-                    $("#downSpeed").on("click", function(){
-                        sprayReader.pause();
-                       wpm = $("#speed").text();
-                       wpm = parseInt(wpm, 10);
-                        if (wpm>150) wpm-=50;
-                        $("#speed").text(wpm);
-                        sprayReader.setWpm(wpm);
-                        sprayReader.resume();
-                   });
-            $("#daBack").on('click', function(){
+    $("#upSpeed").on("click", function(){
+        sprayReader.upSpeed();
+    });
+
+    $("#downSpeed").on("click", function(){
+        sprayReader.downSpeed();
+    });
+        
+    $('#start').click(function() {
+        sprayReader.start(); 
+        event.preventDefault();
+        $("#start").hide();
+        $("#pause").show();
+    });
+    
+    $('#resume').click(function(){
+        sprayReader.resume(); 
+        event.preventDefault();
+        $('#pause').show();
+        $('#start').hide();
+    });
+    
+    $('#pause').click(function() {
+        sprayReader.pause(); 
+        event.preventDefault();
+        $("#pause").hide();
+        $("#resume").show();
+    });
+    
+    $('#stop').click(function() {
+        sprayReader.stop(); 
+        event.preventDefault();
+        $('#pause').hide();
+        $('#resume').hide();
+        $('#start').show();
+    });
+
+    SprayReader.prototype.upSpeed = function(){
+        sprayReader.pause();
+        wpm = $("#speed").text();
+        wpm = parseInt(wpm, 10);
+        if (wpm<850) wpm+=50;
+        $("#speed").text(wpm);
+        sprayReader.setWpm(wpm);
+        sprayReader.resume();
+    }
+
+    SprayReader.prototype.downSpeed = function(){
+        sprayReader.pause();
+        wpm = $("#speed").text();
+        wpm = parseInt(wpm, 10);
+        if (wpm>150) wpm-=50;
+        $("#speed").text(wpm);
+        sprayReader.setWpm(wpm);
+        sprayReader.resume();   
+    }
+    
+    $("#daBack").on('click', function(){
         sprayReader.stop();
         $(".upage-content").hide();
         $("#bookShelf").show();
-                running = false;
-                isPaused = false;
-                $("#start").text("");
-                  $("#start").text("start");
+        running = false;
+        isPaused = false;
+        $("#start").text("");
+        $("#start").text("start");
     });
-            
-            running = false;
-            isPaused = false;
-          $('#start').click(function(event) {
-              if(!running && !isPaused){
-                running=true;
-                isPaused=false;
-                var inputText = text;
-                var wpm = $('#wpm').val();
-    
-                sprayReader.setInput(inputText);
-                sprayReader.setWpm(wpm);
-                sprayReader.start(); 
 
-                event.preventDefault();
-                  $("#start").text("");
-                  $("#start").text("pause");
-              }
-              else if (isPaused && !running){//Resume function
-                    running = true;
-                  isPaused = false;
-                  sprayReader.resume();
-                    event.preventDefault();
-                $("#start").text("");
-                  $("#start").text("pause");
-              }
-              else if (!isPaused && running){
-                running = false; 
-                isPaused = true;
-                sprayReader.pause();
-                event.preventDefault();
-                  $("#start").text(""); 
-                  $("#start").text("start");
-              }
-              else{
-                 running = false;
-                  isPaused = true;
-              }
-          });
 
-          $('#stop').click(function(event) {
-                running = false;
-                isPaused = false;
-                sprayReader.stop();
-                event.preventDefault();
-                $("#start").text("");
-                $("#start").text("start");
-          });
-        });
-        });
-
-        $("#testShelf").append(div);  
+    $("#parse").on('click', function(){
+        //TODO for the parser, needs to return the author, title, content, and word count which will be asynch posted to the database
     });
 });
