e0b922371e9440b1b2d8fc0fddbe25f4bb3cf25e
GeoffreyLong <Geoffrey.Long@mail.mcgill.ca>
2014-03-29 07:23:59 -0400

    start framework for the bookshelf div

--- hackPrinceton/js/functions.js
+++ hackPrinceton/js/functions.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
-    
-    
+    dataElements = [] //This is all the elements of the database
+    data.each(function(elm){
+        author = elm['author'];
+        title = elm['title'];
+        source = elm['source']
+        wordcount = elm['wordcount'];
+        text = elm['text'];
+        
+        div = $("<div class='book'></div>");
+        
+    }
     
 });
